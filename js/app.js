8d2a56af0438b503c84ee054dd40667ce5c41295
juandavid_dev <juandavid.dev@gmail.com>
2023-11-17 19:03:30 -0500

    V.1.2

--- js/app.js
+++ js/app.js
@@ -1,11 +1,24 @@
-// Seleccion de elementos del dom
-document.querySelector('.navbar__toggle-btn').addEventListener('click', function() {
-    
-    // Seleccion del elemento del DOM que tiene la clase 'navbar__mobile-menu'.
-    var mobileMenu = document.querySelector('.navbar__mobile-menu');
-    
-    // Cambia el estilo de visualización del menú móvil.
-    // Si el menú móvil está oculto ('none' o vacío), lo muestra cambiando a 'flex'.
-    // Si el menú móvil ya está visible ('flex'), lo oculta cambiando a 'none'.
-    mobileMenu.style.display = mobileMenu.style.display === 'none' || mobileMenu.style.display === '' ? 'flex' : 'none';
+document.addEventListener("DOMContentLoaded", () => {
+  // Seleccion de elementos del DOM
+  const toggleButton = document.querySelector(".navbar__toggle-btn");
+  const mobileMenu = document.querySelector(".navbar__mobile-menu");
+
+  // Si el menú móvil está oculto ('none' o vacío), lo muestra cambiando a 'flex'.
+  // Si el menú móvil ya está visible ('flex'), lo oculta cambiando a 'none'.
+  const toggleMenu = () => {
+    mobileMenu.style.display =
+      mobileMenu.style.display === "none" || mobileMenu.style.display === ""
+        ? "flex"
+        : "none";
+  };
+
+  // ocultar la lista desplegable
+  const hideMenuOnResize = () => {
+    mobileMenu.style.display = "none";
+  };
+
+  // event listeners
+  toggleButton.addEventListener("click", toggleMenu);
+  window.addEventListener("resize", hideMenuOnResize);
+  window.addEventListener("load", hideMenuOnResize);
 });
